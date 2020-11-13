453d82b43258dd667c68ed7e436052f87009d9c4
Alumy94 <alexeyka.xx@gmail.com>
2020-11-13 19:12:01 +0700

    first commit

--- last_watched_series/main.py
+++ last_watched_series/main.py
@@ -1,10 +1,15 @@
 import psycopg2
+import sys
+import click
 
 
+@click.group()
+@click.version_option("0.1.0")
 def main():
-    print("Start")
+    # CLI for control the last watched series
+    pass
 
 
 if __name__ == '__main__':
+    args = sys.argv
     main()
-
